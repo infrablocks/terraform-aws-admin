dbce9c76236254923f5301bc8dd57c6f75144b4a
Toby Clemson <tobyclemson@gmail.com>
2019-11-12 19:25:33 +0000

    Add admin group membership.

--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -5,6 +5,7 @@ describe 'user' do
   let(:user_arn) {
     output_for(:harness, 'admin_user_arn')
   }
+  let(:group_name) { vars.admin_group_name }
 
   subject {
     iam_user(user_name)
@@ -12,4 +13,6 @@ describe 'user' do
 
   it { should exist }
   its(:arn) { should eq(user_arn) }
+
+  it { should belong_to_iam_group(group_name) }
 end
--- user.tf
+++ user.tf
@@ -1,3 +1,13 @@
 resource "aws_iam_user" "admin" {
   name = var.admin_user_name
 }
+
+resource "aws_iam_group_membership" "admins" {
+  name = "${var.admin_group_name}-group-membership"
+
+  users = [
+    "${aws_iam_user.admin.name}",
+  ]
+
+  group = "${aws_iam_group.admins.name}"
+}
